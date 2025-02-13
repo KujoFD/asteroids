4c5f595a04d7014e4e8e7598334752bc8e400b1f
KujoFD <danny.fulmer5@gmail.com>
2025-02-12 22:00:04 -0800

    EOD1: collision logic implemented, working on shooting. BD C3L3

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
+from player import Player
 from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
+    clock=pygame.time.Clock()
+    dt=0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p1= Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    field= AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for ast in asteroids:
+            if ast.check_collision(p1)==True:
+                print("Game over!")
+                sys.exit()
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
---
+++ player.py
@@ -0,0 +1,43 @@
+import pygame
+from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
+from circleshape import CircleShape
+class Player(CircleShape, pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        pygame.sprite.Sprite.__init__(self)
+        self.rotation = 0
+        self.rect = pygame.Rect(x-PLAYER_RADIUS, y-PLAYER_RADIUS, PLAYER_RADIUS*2, PLAYER_RADIUS*2)
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self,dt):
+        self.rotation+=PLAYER_TURN_SPEED*dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        self.rect.center = self.position
